2be4c59c2e1a4ac42b2b02a4a05a03e1ce516f95
Hoang Nguyen <wallker.night@gmail.com>
2022-10-04 16:04:43 +0700

    add file

--- blsf/main-app/main-app.cpp
+++ blsf/main-app/main-app.cpp
@@ -1,20 +1,21 @@
-// main-app.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "blowfish.h"
 
-int main()
+int main(int argc, const char* argv[])
 {
-    std::cout << "Hello World!\n";
+    unsigned char key[] = "The quick brown fox jumps over the lazy dog.";
+
+    Blowfish blowfish{};
+    blowfish.SetKey(key, sizeof(key));
+
+    // Input/Output length must be a multiple of the block length (64bit)
+    unsigned char text[16] = "Notthing to see";
+
+    blowfish.Encrypt(text, text, sizeof(text));
+    std::cout << text << std::endl;
+
+    blowfish.Decrypt(text, text, sizeof(text));
+    std::cout << text << std::endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
